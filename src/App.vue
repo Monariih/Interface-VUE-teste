280cd63cd72889f8286c746f6f39f25ba97c1c8f
Lucas Monari <lucas.monari.lm@outlook.com>
2023-01-10 15:29:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+
+import {defineComponent} from "vue";
+import IconDocumentation from "@/components/icons/IconDocumentation.vue";
+
+export default defineComponent({
+  components: {IconDocumentation}
+})
+</script>
+
+<template>
+  <header class="cabeçalho">
+    <img id="icon_menu" src="https://cdn-icons-png.flaticon.com/512/1243/1243926.png?w=740&t=st=1673358784~exp=1673359384~hmac=fda79b37e4d733221221de79d9f947768508a5e6cebfa6353ac6bf8e6cb4216f">
+      <div id="escrita">
+
+        <div id="portal">Portal </div>
+        <span id="solar">Solar</span>
+        <img id="logo_cabeçalho" src="https://s3.amazonaws.com/gupy5/production/companies/39836/career/89638/images/2022-09-05_13-27_logo.png">
+
+      </div>
+    </header>
+    <main>
+      <div>
+        <div class="fundo_titulo">
+          Usina - EHF9B78EAI4127 - Lucas Monari dos Santos
+        </div>
+
+        <div id="espaço_usina">
+          <img id="usina" src="https://s2.glbimg.com/WdsHmILtyeubAxY6jSOD5OZNBVI=/0x0:1500x999/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/N/k/B1pS8mTgCUBgKLYcrrTQ/energia-solar-em-casa.jpg">
+        </div>
+
+        <div id="info_energia">
+          <div id="ene_dia">Energia do dia:</div>
+
+          <div id="qnt_energia_num"><spam id="num_energ">50,7</spam>khw</div>
+        <div id="qnt_energia">
+          <img id="icon_energia" src="https://cdn-icons-png.flaticon.com/512/4515/4515788.png">
+        </div>
+        </div>
+        <div id="pos_info">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum doloremque doloribus explicabo hic illo, itaque laudantium modi nisi odit, officiis omnis optio quo sapiente similique sint sit velit, voluptate.
+        </div>
+      </div>
+
+
+  </main>
+</template>
